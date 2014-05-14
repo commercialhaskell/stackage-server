09cf1b10a15266b37b208eb7cefed6ae8081e653
Michael Snoyman <michael@snoyman.com>
2014-05-14 08:59:33 +0300

    Remove some incriminating info from /system

--- Handler/System.hs
+++ Handler/System.hs
@@ -4,7 +4,4 @@ import Import
 import System.Process (readProcess)
 
 getSystemR :: Handler String
-getSystemR = liftIO $ do
-    x <- readProcess "df" ["-ih"] ""
-    y <- readProcess "ls" ["-lh", "/tmp"] ""
-    return $ unlines [x, y]
+getSystemR = liftIO $ readProcess "df" ["-ih"] ""
