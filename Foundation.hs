de112cd75fb484a1cc5a3ccc6fdd5d9402e8ebb4
Chris Done <chrisdone@gmail.com>
2014-06-01 13:29:11 +0200

    Update nav and include bootstrap responsive

--- Foundation.hs
+++ Foundation.hs
@@ -91,6 +91,7 @@ instance Yesod App where
             $(combineStylesheets 'StaticR
                 [ css_normalize_css
                 , css_bootstrap_css
+                , css_bootstrap_responsive_css
                 ])
             $(widgetFile "default-layout")
 
