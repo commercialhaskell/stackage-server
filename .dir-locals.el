841c9f5c816bd2a6da5a68658e4a0fe953b39679
Chris Done <chrisdone@gmail.com>
2014-06-01 13:29:29 +0200

    Add hamlet-mode 4-spaces indentation

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((haskell-mode . ((haskell-process-type . cabal-repl)
                   (haskell-indent-spaces . 4)
-                  (haskell-process-use-ghci . t))))
+                  (haskell-process-use-ghci . t)))
+ (hamlet-mode . ((hamlet/basic-offset . 4))))
