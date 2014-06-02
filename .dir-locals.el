8b945e84300dc97054f1c1ac5bab44694f24954a
Chris Done <chrisdone@gmail.com>
2014-06-02 14:16:21 +0200

    Don't use cabal-repl

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,3 @@
-((haskell-mode . ((haskell-process-type . cabal-repl)
-                  (haskell-indent-spaces . 4)
+((haskell-mode . ((haskell-indent-spaces . 4)
                   (haskell-process-use-ghci . t)))
  (hamlet-mode . ((hamlet/basic-offset . 4))))
