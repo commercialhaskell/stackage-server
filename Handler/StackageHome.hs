11f0d37904d49a60bb5069a898e13eb62adadbc2
Michael Snoyman <michael@snoyman.com>
2014-10-18 19:43:57 +0300

    cabal.config link

--- Handler/StackageHome.hs
+++ Handler/StackageHome.hs
@@ -2,6 +2,7 @@ module Handler.StackageHome where
 
 import Data.BlobStore (storeExists)
 import Import
+import Data.Time (FormatTime)
 
 getStackageHomeR :: PackageSetIdent -> Handler Html
 getStackageHomeR ident = do
@@ -49,4 +50,33 @@ getStackageMetadataR ident = do
         , "\n"
         ]
 
+getStackageCabalConfigR :: PackageSetIdent -> Handler TypedContent
+getStackageCabalConfigR ident = do
+    Entity sid _ <- runDB $ getBy404 $ UniqueStackage ident
+    respondSourceDB typePlain $ stream sid
+  where
+    stream sid =
+        selectSource
+            [ PackageStackage ==. sid
+            , PackageOverwrite ==. False
+            ]
+            [ Asc PackageName'
+            , Asc PackageVersion
+            ] $= (goFirst >> mapC (Chunk . showPackage))
+
+    goFirst = do
+        mx <- await
+        forM_ mx $ \(Entity _ (Package _ name version _)) -> yield $ Chunk $
+            toBuilder (asText "constraints: ") ++
+            toBuilder (toPathPiece name) ++
+            toBuilder (asText " ==") ++
+            toBuilder (toPathPiece version)
+
+    showPackage (Entity _ (Package _ name version _)) =
+        toBuilder (asText ",\n             ") ++
+        toBuilder (toPathPiece name) ++
+        toBuilder (asText " ==") ++
+        toBuilder (toPathPiece version)
+
+yearMonthDay :: FormatTime t => t -> String
 yearMonthDay = formatTime defaultTimeLocale "%Y-%m-%d"
