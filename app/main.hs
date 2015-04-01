6bac8424727106b2d3a5f7c0649c832ba0209aa4
Michael Snoyman <michael@snoyman.com>
2015-04-01 16:59:58 +0300

    Support for --summary (pinging @DanBurton)

--- app/main.hs
+++ app/main.hs
@@ -1,9 +1,13 @@
 import Application (makeApplication)
-import Prelude (IO)
-import Prelude (Bool(..))
+import Prelude (Bool(..), IO, elem, putStrLn)
 import Settings (parseExtra)
 import Yesod.Default.Config (fromArgs)
 import Yesod.Default.Main (defaultMainLog)
+import System.Environment (getArgs)
 
 main :: IO ()
-main = defaultMainLog (fromArgs parseExtra) (makeApplication False)
+main = do
+    args <- getArgs
+    if "--summary" `elem` args
+        then putStrLn "Run the server software for www.stackage.org"
+        else defaultMainLog (fromArgs parseExtra) (makeApplication False)
