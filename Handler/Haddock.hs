24875df4d270ed4a701e03f9b9fbd72a6601c26a
Michael Snoyman <michael@snoyman.com>
2015-05-14 16:47:10 +0300

    Remove warnings in Handler.Haddock

--- Handler/Haddock.hs
+++ Handler/Haddock.hs
@@ -2,22 +2,7 @@ module Handler.Haddock
     ( getHaddockR
     ) where
 
-import           Control.Concurrent (forkIO)
-import           Crypto.Hash (Digest, SHA1)
-import           Crypto.Hash.Conduit (sinkHash)
-import           Data.Aeson (withObject)
-import qualified Data.ByteString.Base16 as B16
-import           Data.Byteable (toBytes)
-import           Data.Conduit.Zlib (gzip)
-import qualified Data.Text as T
-import qualified Data.Yaml as Y
-import           Filesystem (isDirectory, createTree, isFile, rename, removeFile, removeDirectory)
-import qualified Filesystem.Path.CurrentOS as F
-import           Import
-import           Network.Mime (defaultMimeLookup)
-import           System.IO (IOMode (ReadMode), withBinaryFile)
-import           System.IO.Temp (withTempFile)
-import           System.Posix.Files (createLink)
+import Import
 import Stackage.Database
 
 getHaddockR :: SnapName -> [Text] -> Handler ()
