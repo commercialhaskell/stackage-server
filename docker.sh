59fac14f732a30537886d0cb461befd0bb295fc4
Michael Snoyman <michael@snoyman.com>
2015-05-21 09:54:57 +0300

    Include docker stuff

---
+++ docker.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -exu
+
+(
+rm -rf docker/app
+mkdir -p docker/app
+yesod keter
+cd docker/app
+tar xf ../../stackage-server.keter
+cd ..
+docker build -t snoyberg/stackage-server .
+)
