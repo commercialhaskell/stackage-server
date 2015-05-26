f3b81b36b5a04b0ce802725ec73c2e8a6da8a861
Michael Snoyman <michael@snoyman.com>
2015-05-26 07:16:32 +0300

    Better tagging of Docker images

--- docker.sh
+++ docker.sh
@@ -2,6 +2,8 @@
 
 set -exu
 
+TAG=$(git rev-parse --short HEAD)
+
 (
 rm -rf docker/app
 mkdir -p docker/app
@@ -9,6 +11,6 @@ yesod keter
 cd docker/app
 tar xf ../../stackage-server.keter
 cd ..
-docker build -t snoyberg/stackage-server .
-docker push snoyberg/stackage-server
+docker build -t snoyberg/stackage-server:$TAG .
+docker push snoyberg/stackage-server:$TAG
 )
