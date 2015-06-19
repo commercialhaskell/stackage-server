b71e551737ea0cbe06713b0a462398c323b1377e
Michael Snoyman <michael@snoyman.com>
2015-06-19 09:01:53 +0300

    Build Docker images with stack instead of cabal

--- docker.sh
+++ docker.sh
@@ -7,10 +7,10 @@ TAG=$(git rev-parse --short HEAD)
 (
 rm -rf docker/app
 mkdir -p docker/app
-yesod keter
-cd docker/app
-tar xf ../../stackage-server.keter
-cd ..
+stack build
+cp $(stack exec which stackage-server) docker/app
+cp -r static config docker/app
+cd docker
 docker build -t snoyberg/stackage-server:$TAG .
 docker push snoyberg/stackage-server:$TAG
 )
