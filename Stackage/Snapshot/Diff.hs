c43340d40d0ca8980e38bdb1583c74223cc54c72
Konstantin Zudov <konstantin@anche.no>
2015-10-11 06:21:02 +0300

    Documented VersionChange type

--- Stackage/Snapshot/Diff.hs
+++ Stackage/Snapshot/Diff.hs
@@ -18,6 +18,11 @@ type Version = Text
 
 type SnapshotDiff = HashMap PackageName VersionChange
 
+-- | Versions of a package as it occurs in the listings provided to `snapshotDiff`.
+--
+--   Would be represented with `These v1 v2` if the package is present in both listings,
+--   otherwise it would be `This v1` if the package is present only in the first listing,
+--   or `That v2` if only in the second.
 newtype VersionChange = VersionChange { unVersionChange :: These Version Version }
 
 changed :: VersionChange -> Bool
