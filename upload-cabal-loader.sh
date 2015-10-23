a84b598a71d1ee211c66fa7711067eb79ed72594
Michael Snoyman <michael@fpcomplete.com>
2015-10-23 09:48:11 +0000

    Remove some unneeded deployment files

--- upload-cabal-loader.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-
-cabal clean
-cabal configure
-cabal build
-strip dist/build/cabal-loader-stackage/cabal-loader-stackage
-scp dist/build/cabal-loader-stackage/cabal-loader-stackage jenkins.stackage.org:/private
