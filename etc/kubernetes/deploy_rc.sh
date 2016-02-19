bb4bf8d67d0e68f1d9185a2c12a4b6a594c8d0e8
Emanuel Borsboom <manny@fpcomplete.com>
2016-02-19 10:51:27 -0800

    Add comments to deploy script wrappers

--- etc/kubernetes/deploy_rc.sh
+++ etc/kubernetes/deploy_rc.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# See description at https://github.com/fpco/devops-helpers#wrappers
+set -xe
 exec "$(dirname ${BASH_SOURCE[0]})/../common/devops-helpers/kubernetes/deploy_rc_helper.sh" \
      --app "stackage-server" \
      --repo fpco/stackage-server \
