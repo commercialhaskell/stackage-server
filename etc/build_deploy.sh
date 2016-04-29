9cef058b9c76aef7558aafd9863bac981e86b61b
Emanuel Borsboom <manny@fpcomplete.com>
2016-04-29 12:49:02 -0700

    etc/build_deploy.sh: use Docker for build

--- etc/build_deploy.sh
+++ etc/build_deploy.sh
@@ -3,7 +3,7 @@
 set -xe
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
 ENV="$1"; shift
-stack --install-ghc test "$@"
+stack --docker --docker-auto-pull test "$@"
 etc/docker/build.sh --no-build "$@"
 etc/docker/push.sh "$ENV"
 etc/kubernetes/deploy_rc.sh "$ENV"
