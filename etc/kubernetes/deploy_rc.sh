cafb407b2e63daf2ab1ca7668db7f42a55149a04
Emanuel Borsboom <manny@fpcomplete.com>
2016-07-05 08:40:40 -0700

    Update devops-helpers

--- etc/kubernetes/deploy_rc.sh
+++ etc/kubernetes/deploy_rc.sh
@@ -5,5 +5,4 @@ exec "$(dirname ${BASH_SOURCE[0]})/../common/devops-helpers/kubernetes/deploy_rc
      --app "stackage-server" \
      --repo fpco/stackage-server \
      --specdir "$(dirname "${BASH_SOURCE[0]}")" \
-     --clusters ~/.kube/clusters/fpco-prod-us-east-1?/kubeconfig \
      "$@"
