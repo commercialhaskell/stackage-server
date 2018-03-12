edfabd986cb4450c3fed360702ada1945f7b54c6
Michael Snoyman <michael@snoyman.com>
2018-03-12 12:06:41 +0200

    Build Cabal first (so much memory)

--- etc/scripts/stage_docker.sh
+++ etc/scripts/stage_docker.sh
@@ -2,5 +2,6 @@
 set -ex
 cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 mkdir -p etc/docker/_artifacts
+stack build Cabal # Memory
 stack install --local-bin-path=etc/docker/_artifacts "$@"
 rsync -av --delete config static etc/docker/_artifacts/
