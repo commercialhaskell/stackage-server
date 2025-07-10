10c9d8364d0c099d611b02d25620d417e7519362
Bryan Richter <bryan@haskell.foundation>
2025-07-10 09:52:25 +0300

    Split JSON API out of /snapshots
    Fixes #355

--- src/Handler/Snapshots.hs
+++ src/Handler/Snapshots.hs
@@ -3,8 +3,11 @@
 {-# LANGUAGE TupleSections #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE NoFieldSelectors #-}
+{-# LANGUAGE OverloadedRecordDot #-}
+{-# LANGUAGE FlexibleContexts #-}
 
-module Handler.Snapshots where
+module Handler.Snapshots (getAllSnapshotsR, getApiV1SnapshotsR) where
 
 import RIO.Time
 import Import
@@ -13,41 +16,74 @@ import Stackage.Database
 snapshotsPerPage :: Integral a => a
 snapshotsPerPage = 50
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
-getAllSnapshotsR :: Handler TypedContent
-getAllSnapshotsR = track "Handler.Snapshots.getAllSnapshotsR" $ do
+-- | Extracted from an earlier implementation that just used a big tuple.
+data SnapshotInfo = SnapshotInfo
+    { name :: SnapName
+    , title :: Text
+    , prettyDate :: Text
+    } deriving (Eq, Show)
+
+instance ToJSON SnapshotInfo where
+    toJSON (SnapshotInfo name title prettyDate) =
+        array
+            [ toJSON name
+            , toJSON title
+            , toJSON prettyDate
+            ]
+
+-- | Extracted from an earlier implementation that just used a big tuple.
+data Paging = Paging
+    { totalCount :: Int
+    , currentPage :: Int
+    , isFirstPage :: Bool
+    , isLastPage :: Bool
+    } deriving (Eq, Show)
+
+-- | Fetch snapshot data from the DB that is used in these routes.
+fetchSnapshots :: Handler ([[SnapshotInfo]], Paging)
+fetchSnapshots = do
     cacheSeconds $ 60 * 60 * 6
-    now' <- getCurrentTime
+
     currentPageMay <- lookupGetParam "page"
     let currentPage :: Int
         currentPage = fromMaybe 1 (currentPageMay >>= readMay)
+
     totalCount <- countSnapshots Nothing
-    (map entityVal -> snapshots) <-
-        getSnapshots Nothing snapshotsPerPage
-                             ((fromIntegral currentPage - 1) * snapshotsPerPage)
+
+    snapshots <- map entityVal <$>
+        getSnapshots
+            Nothing
+            snapshotsPerPage
+            ((fromIntegral currentPage - 1) * snapshotsPerPage)
+
+    now' <- getCurrentTime
     let groups = groupUp now' snapshots
 
     let isFirstPage = currentPage == 1
         isLastPage = currentPage * snapshotsPerPage >= totalCount
 
-    selectRep $ do
-      provideRep $ defaultLayout $ do
+    pure (groups, Paging totalCount currentPage isFirstPage isLastPage)
+
+  where uncrapify now' snapshot =
+            SnapshotInfo
+                (snapshotName snapshot)
+                (snapshotTitle snapshot)
+                (dateDiff now' (snapshotCreated snapshot))
+        groupUp now' = groupBy (on (==) (.prettyDate))
+                     . map (uncrapify now')
+
+getAllSnapshotsR :: Handler Html
+getAllSnapshotsR = track "Handler.Snapshots.getAllSnapshotsR" $ do
+    (groups, Paging _ currentPage isFirstPage isLastPage) <- fetchSnapshots
+    defaultLayout $ do
         setTitle "Stackage Server"
         let snapshotsNav = $(widgetFile "snapshots-nav")
         $(widgetFile "all-snapshots")
 
-      provideRep $ return $ object ["snapshots" .= groups, "totalCount" .= totalCount]
-
-  where uncrapify now' snapshot =
-            ( snapshotName snapshot
-            , snapshotTitle snapshot
-            , dateDiff now' (snapshotCreated snapshot)
-            )
-        groupUp now' = groupBy (on (==) (\(_,_,uploaded) -> uploaded))
-                     . map (uncrapify now')
+getApiV1SnapshotsR :: Handler Value
+getApiV1SnapshotsR = track "Handler.API.getApiV1SnapshotsR" $ do
+    (groups, paging) <- fetchSnapshots
+    pure $ object
+        [ "snapshots" .= groups
+        , "totalCount" .= paging.totalCount
+        ]
