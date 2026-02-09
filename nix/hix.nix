bb5ae647a59befc8f05198dbf4d39a3360bec279
Andreas Ländle <969523+alaendle@users.noreply.github.com>
2026-02-09 10:39:16 +0100

    Be more precise about the used compiler. Updated flake.

--- nix/hix.nix
+++ nix/hix.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   name = "stackage-server";
-  compiler-nix-name = "ghc910"; # Version of GHC to use
+  compiler-nix-name = "ghc9103"; # Version of GHC to use
 
   # Cross compilation support:
   # crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isx86_64 ([
